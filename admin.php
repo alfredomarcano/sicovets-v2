35b4713a8875d84ea14373c48b3d159fcfae965e
alfredomarcano <alfredomarcan@gmail.com>
2020-02-13 21:20:44 -0400

    primer commit

---
+++ admin.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sicovets - Sistema de Control para Vehiculos de Talleres y Seguros</title>
+	<link rel="stylesheet" href="css/style.css">
+	<link rel="stylesheet" href="css/grid.css">
+	<link rel="stylesheet" href="fontawesome5/css/all.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/css?family=ZCOOL+XiaoWei&display=swap" rel="stylesheet">
+</head>
+<body>
+	<div class="template">
+		<header>
+			<img src="img/logo3.png" alt="">
+			<img src="" alt="">
+		</header>
+		<nav>
+			<ul>
+				<li>
+					<i class="fas fa-tachometer-alt"> </i>
+					<span>Dashboard</span>
+				</li>
+				<li>
+					<i class="fas fa-car"> </i>
+					<span>Revisión</span>
+				</li>
+				<li>
+					<i class="fas fa-office"> </i>
+					<span>Recepción</span>
+				</li>
+				<li>
+					<i class="fas fa-user"> </i>
+					<span>Distribución</span>
+				</li>
+				<li>
+					<i class="fas fa-user"> </i>
+					<span>Estatus</span>
+				</li>
+				<li>
+					<i class="fas fa-user"> </i>
+					<span>Entregados</span>
+				</li>
+				<li>
+					<i class="fas fa-user"> </i>
+					<span>Ordenes</span>
+				</li>
+				<li>
+					<i class="fas fa-user"> </i>
+					<span>Cotizaciones</span>
+				</li>
+				<li>
+					<i class="fas fa-user"> </i>
+					<span>Respuesto</span>
+				</li>
+			</ul>
+		</nav>
+		<section>
+			<div class="route">
+				<div class="separador">
+					<h4>Dashboard</h4>
+				</div>
+			</div>
+			<div class="separador">
+				<div class="top_general">
+					<div class="top_nav">
+						<ul>
+							<li>Recibidos</li>
+							<li>Entregados</li>
+							<li>Ordenes</li>
+							<li>Repuestos</li>
+						</ul>
+					</div>
+					<div class="top_recibidos">
+						<p>Recepcion</p>
+					</div>
+					<div class="top_despacho">
+						<p>Despacho</p>
+					</div>
+					<div class="top_latoneria">
+						<p>Latoneria</p>
+					</div>
+					<div class="top_pintura">
+						<p>Pitura</p>
+					</div>
+					<div class="top_mecanica">
+						<p>Mecanica</p>
+					</div>
+					<div class="top_pulitura">
+						<p>Pulitura</p>
+					</div>
+				</div>
+			</div>
+		</section>
+		<footer>
+			<ul>
+				<li>lorem</li>
+				<li>lorem</li>
+				<li>lorem</li>
+				<li>lorem</li>
+			</ul>
+		</footer>
+	</div>
+</body>
+</html>
